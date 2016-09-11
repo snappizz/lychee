189d29a6cd8678327b3f8d177d66973cc5d7e17b
Nathan Ho <nathan@snappizz.com>
2016-09-11 16:37:50 -0700

    New: round-trip LilyPond/LMEI conversion scripts
    These scripts convert documents between LilyPond/LMEI and back. They are for use by developers working on converters for these formats to check for information loss and other discrepancies during the conversion process. Like other helper scripts, they do not require going through a Lychee action for use.
    
    For T133.

---
+++ scripts/lmei_to_ly_to_lmei.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Program Name:           Lychee
+# Program Description:    MEI document manager for formalized document control
+#
+# Filename:               scripts/ly_to_lmei_to_ly.py
+# Purpose:                Converts an MEI document to LilyPond and back.
+#
+# Copyright (C) 2016 Nathan Ho
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------
+'''
+Converts an MEI document to LilyPond and back.
+'''
+
+from lxml import etree
+from lychee.converters.inbound import lilypond as inbound_lilypond
+from lychee.converters.outbound import lilypond as outbound_lilypond
+
+
+def ly_to_lmei_to_ly(lmei_string):
+    mei_thing = etree.fromstring(lmei_string)
+    lilypond_thing = outbound_lilypond.convert(mei_thing)
+    converted_mei_thing = inbound_lilypond.convert(lilypond_string)
+    converted_lmei_string = etree.tostring(converted_lmei_string, pretty_print=True)
+    return converted_lmei_string
